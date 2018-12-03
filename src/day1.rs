bd73a63a0a0892f6ad04beac4abc82a14b6d31de
Ryan P. Brewster <RyanPBrewster@gmail.com>
2018-12-02 20:55:51 -0800

    day1/part2

--- src/day1.rs
+++ src/day1.rs
@@ -1,12 +1,23 @@
-fn main() {
-    println!("Hello, World!");
+use std::collections::HashSet;
+
+fn find_first_duplicate(xs: &[i32]) -> i32 {
+    let mut cur = 0;
+    let mut seen = HashSet::new();
+    loop {
+        for x in xs {
+            if seen.contains(&cur) {
+                return cur;
+            }
+            seen.insert(cur);
+            cur += x;
+        }
+    }
 }
 
 #[cfg(test)]
 mod test {
+    use super::*;
     use std::fs;
-    use std::io::Read;
-    use std::path::Path;
 
     #[test]
     fn part1() {
@@ -16,5 +27,15 @@ mod test {
             .map(|s| s.parse::<i32>().expect("parse line to i32"))
             .sum();
         assert_eq!(total, 402);
+    }
+
+    #[test]
+    fn part2() {
+        let contents = fs::read_to_string("data/day1/input").expect("read input file");
+        let inputs: Vec<i32> = contents
+            .lines()
+            .map(|s| s.parse::<i32>().expect("parse line to i32"))
+            .collect();
+        assert_eq!(find_first_duplicate(&inputs), 481);
     }
 }
