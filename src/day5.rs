df628e627cbbb62ee31c9982172087ba17f41497
Ryan P. Brewster <RyanPBrewster@gmail.com>
2018-12-13 23:12:06 -0800

    day5/part1, implemented poorly

---
+++ src/day5.rs
@@ -0,0 +1,34 @@
+fn compact(input: &str) -> String {
+    let mut chars: Vec<char> = input.chars().collect();
+    let mut i = 0;
+    while i + 1 < chars.len() {
+        if chars[i].to_ascii_lowercase() == chars[i + 1].to_ascii_lowercase()
+            && chars[i] != chars[i + 1]
+        {
+            chars.remove(i);
+            chars.remove(i);
+            if i > 0 {
+                i -= 1;
+            }
+        } else {
+            i += 1;
+        }
+    }
+    chars.into_iter().collect()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use lazy_static::lazy_static;
+    use std::fs;
+
+    lazy_static! {
+        static ref INPUT: String = fs::read_to_string("data/day5/input").expect("read input file");
+    }
+
+    #[test]
+    fn part1() {
+        assert_eq!(compact(INPUT.trim()).len(), 11118);
+    }
+}
