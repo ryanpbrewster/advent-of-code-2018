5e09276f53f002fec5f24241f010c0bb5c5c0a9e
Ryan P. Brewster <RyanPBrewster@gmail.com>
2019-01-08 00:16:32 -0800

    simpler day8/part1

--- src/day8.rs
+++ src/day8.rs
@@ -3,24 +3,26 @@ struct Node {
     children: Vec<Node>,
 }
 impl Node {
-    fn for_each<F>(&self, f: &mut F) where F: FnMut(&[usize]) {
-        f(&self.metadata);
-        for child in &self.children {
-            child.for_each(f);
-        }
-    }
-    fn from_iter<'a, I>(value: &mut I) -> Result<Self, ()> where I: Iterator<Item = &'a usize> {
+    fn from_iter<'a, I>(value: &mut I) -> Result<Self, ()>
+    where
+        I: Iterator<Item = &'a usize>,
+    {
         let num_children = *value.next().ok_or(())?;
         let num_metadata = *value.next().ok_or(())?;
         let mut children = Vec::new();
-        for _ in 0 .. num_children {
+        for _ in 0..num_children {
             children.push(Node::from_iter(value)?);
         }
         let mut metadata = Vec::new();
-        for _ in 0 .. num_metadata {
+        for _ in 0..num_metadata {
             metadata.push(*value.next().ok_or(())?);
         }
         Ok(Node { metadata, children })
+    }
+
+    fn simple_sum(&self) -> usize {
+        self.metadata.iter().sum::<usize>()
+            + self.children.iter().map(|c| c.simple_sum()).sum::<usize>()
     }
 }
 
@@ -33,30 +35,23 @@ mod test {
     lazy_static! {
         static ref INPUT: Vec<usize> = {
             let raw = fs::read_to_string("data/day8/input").expect("read input file");
-            raw.split_whitespace()
-                .map(|w| w.parse().unwrap())
-                .collect()
+            raw.split_whitespace().map(|w| w.parse().unwrap()).collect()
         };
     }
 
     #[test]
     fn smoke() {
-        let input = vec![2,3,0,3,10,11,12,1,1,0,1,99,2,1,1,2];
+        let input = vec![2, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 1, 1, 2];
         let mut iter = input.iter();
         let node = Node::from_iter(&mut iter).unwrap();
 
-        let mut total = 0;
-        node.for_each(&mut |md| total += md.iter().sum::<usize>());
-        assert_eq!(total, 138);
+        assert_eq!(node.simple_sum(), 138);
     }
 
     #[test]
     fn part1() {
         let mut iter = INPUT.iter();
         let node = Node::from_iter(&mut iter).unwrap();
-
-        let mut total = 0;
-        node.for_each(&mut |md| total += md.iter().sum::<usize>());
-        assert_eq!(total, 42254);
+        assert_eq!(node.simple_sum(), 42254);
     }
 }
